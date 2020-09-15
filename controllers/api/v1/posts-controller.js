441e069a4f87980c1ab1c624b20571bbe448a3c3
Sonu <sonu3660@gmail.com>
2020-09-16 02:57:15 +0530

    Add comment to posts

--- controllers/api/v1/posts-controller.js
+++ controllers/api/v1/posts-controller.js
@@ -56,14 +56,14 @@ module.exports.deletePost = (req, res) => {
             if (post.user.toString() !== req.user.id) {
                return res
                   .status(401)
-                  .json({ notauthorized: "User not authorized" });
+                  .json({ notAuthorized: "User not authorized" });
             }
 
             // Delete
             post.remove().then(() => res.json({ success: true }));
          })
          .catch((err) =>
-            res.status(404).json({ postnotfound: "No post found" })
+            res.status(404).json({ postNotFound: "No post found" })
          );
    });
 };
