5a8bc1710a1a5be5264a8ed552b379f6c7aa9b9d
Sonu <sonu3660@gmail.com>
2020-09-21 20:50:02 +0530

    Added component for education, experience detail for developers profile

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import setAuthToken from "./redux/utils/setAuthToken";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
+
 import AddExperience from "./components/profile-form/AddExperience";
 import AddEducation from "./components/profile-form/AddEducation";
 
@@ -46,7 +48,7 @@ const App = () => {
                      <Route exact path="/register" component={Register} />
                      <Route exact path="/login" component={Login} />
                      <Route exact path="/profiles" component={Profiles} />
-
+                     <Route exact path="/profile/:id" component={Profile} />
                      <PrivateRoute
                         exact
                         path="/dashboard"
