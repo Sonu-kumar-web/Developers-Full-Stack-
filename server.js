db8f7c8cd0375ee3dec003a25eb48d7abe35a017
Sonu <sonu3660@gmail.com>
2020-09-22 15:02:56 +0530

    Final production

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const db = require("./config/mongoose");
+const path = require("path");
 const passportJWT = require("./config/passport-jwt-strategy");
 const cors = require("cors");
 const app = express();
@@ -16,6 +17,16 @@ const port = process.env.PORT || 5000;
 
 // Use express routes
 app.use("/", require("./routes/index.js"));
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+   // Set static folder
+   app.use(express.static("client/build"));
+
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
 
 app.listen(port, (err) => {
    if (err) {
